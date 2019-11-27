f5f27254fca1d5547088cfc525ac3ae0d6aead5c
Aneudy Abreu <aneudy91@gmail.com>
2019-11-27 03:48:26 +0000

    Puesta a punto

--- index.js
+++ index.js
@@ -14,10 +14,11 @@ var obj = {
     //   },
     Expediciones : [
          {
+           // Esta es la expedición de Recogida
             Expedicion : {
-                ClienteRemitente                : 'Terraquo_test',
-                MailRemitente                   : 'aneudyabreucolon@gmail.com',
-                EnviarMail                      : 'S',
+                ClienteRemitente                : '00800025-01',
+                // MailRemitente                   : 'aneudyabreucolon@gmail.com',
+                // EnviarMail                      : 'S',
                 NombreDestinatario              : 'Jose López',
                 PersonaContactoDestinatario     : 'Jose López',
                 DireccionDestinatario           : 'Lorem ipsum dolor sit amet, consectetur adipisicing elit',
@@ -25,20 +26,22 @@ var obj = {
                 CodigoPostalDestinatario        : '99999',
                 TelefonoContactoDestinatario    : '+5219999999999',
                 ReferenciaCliente               : 'TERRAQUO',
+                // Fecha                           : '26/11/2019',
                 Bultos                          : '1',
                 Kilos                           : '1',
                 ImporteReembolso                : '0',
-                ProductoServicio                : 'Pizzas',
-                Observaciones1                  : 'Ninguna',
-                Observaciones2                  : 'Ninguna',
+                ProductoServicio                : '80',
+                Observaciones1                  : '',
+                Observaciones2                  : '',
                 MailDestinatario                : 'aneudy91@gmail.com',
             }
         }
         ,{
             Expedicion : {
-                ClienteRemitente                : 'Terraquo_test',
-                MailRemitente                   : 'aneudyabreucolon@gmail.com',
-                EnviarMail                      : 'S',
+                ClienteRemitente                : '00800025-01',
+                NombreRemitente                 : 'Jose López',
+                // MailRemitente                   : 'aneudyabreucolon@gmail.com',
+                // EnviarMail                      : 'S',
                 NombreDestinatario              : 'Andrés Manuel',
                 PersonaContactoDestinatario     : 'Andrés Manuel',
                 DireccionDestinatario           : 'Lorem ipsum dolor sit amet, consectetur adipisicing elit',
@@ -46,19 +49,20 @@ var obj = {
                 CodigoPostalDestinatario        : '99999',
                 TelefonoContactoDestinatario    : '+5219999999999',
                 ReferenciaCliente               : 'TERRAQUO',
+                // Fecha                           : '26/11/2019',
                 Bultos                          : '1',
-                Kilos                           : '1',
+                Kilos                           : '1.0',
                 ImporteReembolso                : '0',
-                ProductoServicio                : 'Pizzas',
-                Observaciones1                  : 'Ninguna',
-                Observaciones2                  : 'Ninguna',
+                ProductoServicio                : '10',
+                Observaciones1                  : '',
+                Observaciones2                  : '',
                 MailDestinatario                : 'aneudy91@gmail.com',
             }
         }
     ]
 };
  
-var builder = new xml2js.Builder();
+var builder = new xml2js.Builder({xmldec :{encoding:"ISO-8859-1",standalone:"yes"}});
 var xml = builder.buildObject(obj);
 
 var finalXML = `
@@ -66,17 +70,17 @@ var finalXML = `
    <soapenv:Header/>
    <soapenv:Body>
       <web:entrada_expediciones>
-         <cliente>CODIGO_CLIENTE</cliente>
+         <cliente>00800025-01</cliente>
          <formato>XML</formato>
          <fichero><![CDATA[${xml}]]>
-		</fichero>
+		    </fichero>
       </web:entrada_expediciones>
    </soapenv:Body>
 </soapenv:Envelope>
 `
-console.log(finalXML)
+// console.log(finalXML)
 
-fs.writeFile('expediciones.xml', finalXML, function(err, data) {
+fs.writeFile('expediciones2.xml', finalXML, function(err, data) {
     if (err) {
       console.log(err);
     }
@@ -85,22 +89,34 @@ fs.writeFile('expediciones.xml', finalXML, function(err, data) {
     }
   });
 
-
 const soapRequest = require('easy-soap-request');
- 
+const username = 'ANEUDY', passw ='foodsterraquo'
 // example data
 const url = 'https://geeverpre.alertran.net/geeverpre/ws_clientes?wsdl';
 const sampleHeaders = {
   'user-agent': 'terraquo_test',
   'Content-Type': 'text/xml;charset=UTF-8',
-//   'soapAction': 'https://graphical.weather.gov/xml/DWMLgen/wsdl/ndfdXML.wsdl#LatLonListZipCode',
+  'Authorization': 'Basic ' + new Buffer(username + ':' + passw).toString('base64'),
+  // 'soapAction': 'set_incidencia',
 };
  
 // usage of module
 (async () => {
   const { response } = await soapRequest({ url: url, headers: sampleHeaders, xml: finalXML, timeout: 10000 }); // Optional timeout parameter(milliseconds)
   const { headers, body, statusCode } = response;
-  console.log(headers);
+  // console.log(headers);
+  console.log('body');
   console.log(body);
+  console.log('statusCode');
   console.log(statusCode);
+
+  fs.writeFile('respuesta1.xml', JSON.stringify(body), function(err, data) {
+    if (err) {
+      console.log(err);
+    }
+    else {
+      console.log('respuesta created!');
+    }
+  });
+
 })();
