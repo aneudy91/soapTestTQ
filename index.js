794d71953f292cd567212ba60e18bc15f0c3eae5
Aneudy Abreu <aneudy91@gmail.com>
2019-12-03 03:55:45 +0000

    Extract info

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
-var xml2js = require('xml2js');
+const xml2js = require('xml2js');
+const parseString = require('xml2js').parseString;
 const fs = require('fs');
  
 var obj = { 
@@ -22,9 +23,9 @@ var obj = {
                 NombreDestinatario              : 'Jose López',
                 PersonaContactoDestinatario     : 'Jose López',
                 DireccionDestinatario           : 'Lorem ipsum dolor sit amet, consectetur adipisicing elit',
-                PoblacionDestinatario           : 'Lorem ipsum',
-                CodigoPostalDestinatario        : '99999',
-                TelefonoContactoDestinatario    : '+5219999999999',
+                PoblacionDestinatario           : 'BARCELONA',
+                CodigoPostalDestinatario        : '08001',
+                TelefonoContactoDestinatario    : '5219999999999',
                 ReferenciaCliente               : 'TERRAQUO',
                 // Fecha                           : '26/11/2019',
                 Bultos                          : '1',
@@ -45,9 +46,9 @@ var obj = {
                 NombreDestinatario              : 'Andrés Manuel',
                 PersonaContactoDestinatario     : 'Andrés Manuel',
                 DireccionDestinatario           : 'Lorem ipsum dolor sit amet, consectetur adipisicing elit',
-                PoblacionDestinatario           : 'Lorem ipsum',
-                CodigoPostalDestinatario        : '99999',
-                TelefonoContactoDestinatario    : '+5219999999999',
+                PoblacionDestinatario           : 'BARCELONA',
+                CodigoPostalDestinatario        : '08001',
+                TelefonoContactoDestinatario    : '5219999999999',
                 ReferenciaCliente               : 'TERRAQUO',
                 // Fecha                           : '26/11/2019',
                 Bultos                          : '1',
@@ -80,7 +81,9 @@ var finalXML = `
 `
 // console.log(finalXML)
 
-fs.writeFile('expediciones2.xml', finalXML, function(err, data) {
+parseString(finalXML, function (err, result) {
+  console.dir(result);
+  fs.writeFile('expediciones.json', JSON.stringify(result), function(err, data) {
     if (err) {
       console.log(err);
     }
@@ -88,6 +91,15 @@ fs.writeFile('expediciones2.xml', finalXML, function(err, data) {
       console.log('updated!');
     }
   });
+});
+// fs.writeFile('expediciones2.xml', finalXML, function(err, data) {
+//     if (err) {
+//       console.log(err);
+//     }
+//     else {
+//       console.log('updated!');
+//     }
+//   });
 
 const soapRequest = require('easy-soap-request');
 const username = 'ANEUDY', passw ='foodsterraquo'
@@ -110,13 +122,34 @@ const sampleHeaders = {
   console.log('statusCode');
   console.log(statusCode);
 
-  fs.writeFile('respuesta1.xml', JSON.stringify(body), function(err, data) {
-    if (err) {
-      console.log(err);
-    }
-    else {
-      console.log('respuesta created!');
-    }
+  parseString(body, function (err, result) {
+    console.log(result);
+
+    let str = result['S:Envelope']['S:Body'][0]['ns2:entrada_expedicionesResponse'][0].return[0]
+    let str2 = str.split(',')
+    
+    let idExpedicion1 = str2[0].split(' ')[1]
+    let idExpedicion2 = str.split(',')[1]
+    let respuesta = str2[0].split(' ')[0]
+    console.log(idExpedicion1)
+    console.log(idExpedicion2)
+    console.log(respuesta)
+    fs.writeFile('response.json', JSON.stringify(result), function(err, data) {
+      if (err) {
+        console.log(err);
+      }
+      else {
+        console.log('updated!');
+      }
+    });
   });
+  // fs.writeFile('respuesta1.xml', JSON.stringify(body), function(err, data) {
+  //   if (err) {
+  //     console.log(err);
+  //   }
+  //   else {
+  //     console.log('respuesta created!');
+  //   }
+  // });
 
 })();
--- index2.js
+++ index2.js
@@ -1,21 +1,33 @@
-var xml2js = require('xml2js');
+var parseString = require('xml2js').parseString;
 const fs = require('fs');
 const soapRequest = require('easy-soap-request');
 
+var xml = "<root>Hello xml2js!</root>"
+parseString(xml, function (err, result) {
+    console.dir(result);
+    fs.writeFile('response.json', JSON.stringify(result), function(err, data) {
+      if (err) {
+        console.log(err);
+      }
+      else {
+        console.log('updated!');
+      }
+    });
+});
 
-// example data
-const url = 'http://localhost/WcfService1/WebService1.asmx?WSDL';
-const sampleHeaders = {
-  'user-agent': 'terraquo_test',
-  'Content-Type': 'text/xml;charset=UTF-8',
-  'soapAction': 'http://localhost/WcfService1/HelloWorld',
-};
+// // example data
+// const url = 'http://localhost/WcfService1/WebService1.asmx?WSDL';
+// const sampleHeaders = {
+//   'user-agent': 'terraquo_test',
+//   'Content-Type': 'text/xml;charset=UTF-8',
+//   'soapAction': 'http://localhost/WcfService1/HelloWorld',
+// };
  
-// usage of module
-(async () => {
-  const { response } = await soapRequest({ url: url, headers: sampleHeaders,  timeout: 10000 }); // Optional timeout parameter(milliseconds)
-  const { headers, body, statusCode } = response;
-  console.log(headers);
-  console.log(body);
-  console.log(statusCode);
-})();
+// // usage of module
+// (async () => {
+//   const { response } = await soapRequest({ url: url, headers: sampleHeaders,  timeout: 10000 }); // Optional timeout parameter(milliseconds)
+//   const { headers, body, statusCode } = response;
+//   console.log(headers);
+//   console.log(body);
+//   console.log(statusCode);
+// })();
